6917401beff70c55ebb5145898aeb47fc3f1c2ce
Joe Mazzone <40954645+MrMazzone@users.noreply.github.com>
2019-08-13 22:04:30 -0400

    Update Camera_with_DateTime_Filename.py

--- Camera_with_DateTime_Filename.py
+++ Camera_with_DateTime_Filename.py
@@ -8,6 +8,6 @@ camera.start_preview(alpha=50)
 
 time.sleep(1)
 
-camera.capture(datetime.datetime.now().strftime("%m-%d-%y_%H-%M") + ".jpg")
+camera.capture(datetime.datetime.now().strftime("%m-%d-%y_%H-%M-%S") + ".jpg")
 
 camera.stop_preview()
